873634c2ee54f0025e08478862decb529b46910e
myfunc <mironov.link@gmail.com>
2023-10-12 00:12:03 -0700

    Added env recognition for launch chrome

--- services/core/src/integrations/kick/kick.service.ts
+++ services/core/src/integrations/kick/kick.service.ts
@@ -3,6 +3,7 @@ import { Page } from "puppeteer-extra-plugin/dist/puppeteer";
 import { Injectable } from "@nestjs/common";
 import WebSocket from "ws";
 import { ChannelData, ChatMessage } from "./dto";
+import { Browser } from "puppeteer";
 
 export type OnMessageHandler = (message: ChatMessage) => void;
 export type WSConnection = {
@@ -26,11 +27,17 @@ export class KickService {
     public async init() {
         if (this._page) return;
 
-        const browser = await puppeteer.launch({
-            headless: true,
-            executablePath: "/usr/bin/google-chrome-stable",
-            args: ["--no-sandbox"], // Disable the Chrome sandbox
-        });
+        let browser: Browser;
+        if (process.env.RUN_IN_DOCKER) {
+            browser = await puppeteer.launch({
+                headless: true,
+                executablePath: "/usr/bin/google-chrome-stable",
+                args: ["--no-sandbox"], // Disable the Chrome sandbox
+            });
+        } else {
+            browser = await puppeteer.launch();
+        }
+
         this._page = await browser.newPage();
         await this._page.setViewport({ width: 1920, height: 1080 });
         await this._page.goto(this.kickUrl);
@@ -46,14 +53,18 @@ export class KickService {
     }
 
     public async getDataByMethod<T>(endpoint) {
-        const page = await this.getPage();
-        const responseData = await page.evaluate((endpoint) => {
-            return fetch(endpoint).then(
-                (response) => response.json() as Promise<T>
-            );
-        }, endpoint);
+        try {
+            const page = await this.getPage();
+            const responseData = await page.evaluate((endpoint) => {
+                return fetch(endpoint).then(
+                    (response) => response.json() as Promise<T>
+                );
+            }, endpoint);
 
-        return responseData;
+            return responseData;
+        } catch (e) {
+            console.error(`Error on fetch data: ${e.message}, ${e.stack}`);
+        }
     }
 
     public async getChannelInfo(channelName) {
